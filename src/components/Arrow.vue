7f80cba3d9ffe1cd33508481a40e47a9343fe5e7
P-P-P-Pablo <pvilella.pro@gmail.com>
2023-02-09 13:38:05 +0100

    trying to m

---
+++ src/components/Arrow.vue
@@ -0,0 +1,56 @@
+<script setup>
+defineProps({
+    width: Number,
+    length: Number,
+    rotation: Number,
+    scaleMax: Number,
+    scaleMin: Number
+})
+</script>
+
+<template>
+    <div class="arrow" :style="`--width: ${width}px; --length: ${length}px; --rotation: ${rotation}deg; --scalemax: ${scaleMax}; --scalemin: ${scaleMin};`">
+        <div class="arrow-body"></div>
+        <div class="arrow-point"></div>
+    </div>
+</template>
+
+<style scoped>
+.arrow {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    width: var(--width);
+    height: var(--length);
+    animation: pulse 1s infinite ease-in-out alternate;
+}
+
+.arrow-point {
+    width: 0;
+    height: 0;
+    border-left: calc(0.4 * var(--width)) solid transparent;
+    border-right: calc(0.4 * var(--width)) solid transparent;
+    border-top: calc(0.5 * var(--length)) solid var(--mauve);
+    filter: drop-shadow(8px 10px 4px rgba(100, 100, 100, 0.3));
+}
+
+.arrow-body {
+    width: 40%;
+    height: 66%;
+    background-color: var(--mauve);
+    box-shadow: 8px 10px 4px rgba(100, 100, 100, 0.3);
+}
+
+
+
+@keyframes pulse {
+    from {
+        transform: scale(var(--scalemin)) rotate(var(--rotation));
+    }
+
+    to {
+        transform: scale(var(--scalemax)) rotate(var(--rotation));
+    }
+}
+</style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+import Arrow from '../components/Arrow.vue';
+
+</script>
 
 <template>
     <div class="main-body">
-        <h1>Not available yet</h1>
+        <Arrow class="little" width="50" length="50" rotation="-45" scale-max="1.1" scale-min="0.9"/>
+        <Arrow class="medium" width="100" length="100" rotation="45" scale-max="1.1" scale-min="0.9"/>
+        <Arrow class="big" width="150" length="150" rotation="90" scale-max="1.1" scale-min="0.9"/>
     </div>
 </template>
