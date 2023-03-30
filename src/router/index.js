4c4c5cea3c1eb2140469afc8900b73f20c9dbecb
P-P-P-Pablo <pvilella.pro@gmail.com>
2023-03-30 12:21:23 +0200

    need to push in prod for sharing with testers

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/HomeView.vue";
-import Projects from "../views/ProjectsView.vue";
-import CV from "../views/CVView.vue";
 import Window from "../views/WindowView.vue";
 
 const routes = [
-  { path: "/", component: Window },
-  { path: "/cv", component: CV },
-  { path: "/projects", component: Projects },
-  { path: "/test", component: Home },
+  { path: "/home", component: Window },
+  { path: "/", component: Home },
 ];
 
 const router = createRouter({
